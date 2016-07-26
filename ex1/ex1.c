5c4e8b057028bf9daf434d16483697de8247f42e
Scott Edwards <scott.edwards3@gmail.com>
2016-07-26 17:49:32 -0400

    exercises 1 and 2

---
+++ ex1/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    puts("/-------------\\");
+    puts("|Hello, world.|");
+    puts("\\-------------/");
+    puts("Second line.");
+    puts("Third line.");
+    puts("Fourth line is\n\ttwo lines.");
+
+    return 0;
+}
